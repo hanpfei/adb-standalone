beb6e2a8dc6957af63799aa58a6cf7abde311c9f
Han Pengfei <hanpfei@gmail.com>
2017-11-08 18:52:31 +0800

    Update script.
    Signed-off-by: Han Pengfei <hanpfei@gmail.com>

--- adb-download-make.sh
+++ adb-download-make.sh
@@ -6,6 +6,7 @@ branch=android-7.1.1_r22
 
 # Makefile to use (will be automatically copied into system/core/adb)
 makefile=makefile.sample
+liblogmakefile=makefile.liblog.sample
 
 
 # DOWNLOAD necessary files
@@ -16,22 +17,25 @@ mkdir android-adb
 cd android-adb
 mkdir system
 cd system
-git clone -b $branch https://android.googlesource.com/platform/system/core
-git clone -b $branch https://android.googlesource.com/platform/system/extras
+git clone -b adb_on_arm https://github.com/hanpfei/android_system_core
+#git clone -b $branch https://android.googlesource.com/platform/system/extras
 cd ..
-mkdir external
-cd external
-git clone -b $branch https://android.googlesource.com/platform/external/zlib
-git clone -b $branch https://android.googlesource.com/platform/external/openssl
-git clone -b $branch https://android.googlesource.com/platform/external/libselinux
-cd ..
+#mkdir external
+#cd external
+#git clone -b $branch https://android.googlesource.com/platform/external/zlib
+#git clone -b $branch https://android.googlesource.com/platform/external/openssl
+#git clone -b $branch https://android.googlesource.com/platform/external/libselinux
+#cd ..
 
 
 # MAKE
 # -------------------------
 echo "\n>> Copying makefile into system/core/adb...\n"
 cp ../$makefile system/core/adb/makefile -f
-cd system/core/adb/
+cp ../$liblogmakefile system/core/liblog/makefile -f
+cd system/core/liblog/
+make
+cd ../adb/
 echo "\n>> Make... \n"
 make clean
 make
